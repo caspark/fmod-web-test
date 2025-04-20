ec0e01596136608e9af1d4e5656554f3404564bb
Caspar Krieger <git@asparck.com>
2025-04-20 20:35:47 +0800

    Hack together playing sounds from rust

--- src/web.rs
+++ src/web.rs
@@ -1,18 +1,69 @@
+use std::cell::RefCell;
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = window)]
+    fn playEvent(sound_id: i32);
+
+    #[wasm_bindgen(js_namespace = window)]
+    fn isFmodReady() -> bool;
+}
 
 #[cfg(target_arch = "wasm32")]
 pub fn run() -> Result<(), JsValue> {
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // window object.
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
+    let f = Rc::new(RefCell::new(None));
+    let g = f.clone();
 
-    // Manufacture the element we're gonna append
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust 4!");
+    let mut i = 0;
+    *g.borrow_mut() = Some(Closure::new(move || {
+        if i > 1000 {
+            body().set_text_content(Some("All done!"));
 
-    body.append_child(&val)?;
+            // Drop our handle to this closure so that it will get cleaned
+            // up once we return.
+            let _ = f.borrow_mut().take();
+            return;
+        }
 
+        // Set the body's text content to how many times this
+        // requestAnimationFrame callback has fired.
+        i += 1;
+        let text = format!("requestAnimationFrame has been called {} times.", i);
+        body().set_text_content(Some(&text));
+
+        // Schedule ourself for another requestAnimationFrame callback.
+        request_animation_frame(f.borrow().as_ref().unwrap());
+
+        if isFmodReady() && i % 100 == 0 {
+            playEvent(0);
+        }
+    }));
+
+    request_animation_frame(g.borrow().as_ref().unwrap());
     Ok(())
+}
+
+fn window() -> web_sys::Window {
+    web_sys::window().expect("no global `window` exists")
+}
+
+fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
+}
+
+fn document() -> web_sys::Document {
+    window()
+        .document()
+        .expect("should have a document on window")
+}
+
+fn body() -> web_sys::Element {
+    document()
+        .query_selector("#output")
+        .expect("query should be valid")
+        .expect("document should have an #output element")
 }
